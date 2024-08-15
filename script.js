478319edbd82210424478009f4a331df41cb9f51
Matthew <158129347+Montoose@users.noreply.github.com>
2024-08-14 22:04:37 -0500

    Create main playing board
    A board that consists of equal number of rows
    and columns consisting of small squares. Each
    square is equally sized. Size of the board depends
    on input into function.

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
+let container = document.getElementById("container");
 
 function createGrid (numberDivs) {
-    let container = document.getElementById("container");
+
     for (i=0; i < numberDivs; i++) {
-        let square = container.appendChild(document.createElement("div"));
+        let row = container.appendChild(document.createElement("div"));
+        row.setAttribute("class", "row");
+        row.style.display = "flex";
+        row.style.flexGrow = 1;
+        
+        for (j=0; j < numberDivs; j++) {
+            let square = row.appendChild(document.createElement("div"));
+            square.setAttribute("class", "square");
+            square.style.flexGrow = "1";
+            square.style.height = "100%";
+        }
     }
 }
 
-createGrid(24);
+createGrid(30);
