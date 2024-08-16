19bf79244ef5cb864fc0dfe8c2d1dfeeb397de96
Matthew <158129347+Montoose@users.noreply.github.com>
2024-08-16 17:19:19 -0500

    Add rainbow mode and opacity feature
    Add rainbow mode which allows you to add a
    different color each time you hover over a square.
    
    Add opacity feature that makes squares start at
    low opacity and then progressively darken as you
    hover over them.

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ const slider = document.getElementById("sizeSlider");
 const container = document.getElementById("container");
 const resetButton = document.getElementById("resetButton");
 const randomButton = document.getElementById("randomColor");
+const rainbowButton = document.getElementById("rainbowColor");
+const regularModeButton = document.getElementById("regularMode");
 
 let hoverColor = "black";
+let rainbowMode = false;
 
-let createGrid = function (numberDivs) {
+function createGrid (numberDivs) {
     let container = document.getElementById("container");
 
     for (i=0; i < numberDivs; i++) {
@@ -17,10 +20,19 @@ let createGrid = function (numberDivs) {
         for (j=0; j < numberDivs; j++) {
             let square = row.appendChild(document.createElement("div"));
             square.setAttribute("class", "square");
+            square.style.backgroundColor = "white";
             square.style.flexGrow = "1";
             square.style.height = "100%";
+            square.style.opacity = 0.1;
         }
     }
+}
+
+function pickRandomColor() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    hoverColor = `rgb(${r}, ${g}, ${b})`;
 }
 
 window.addEventListener("load", () => {
@@ -33,18 +45,38 @@ slider.addEventListener("click", () => {
     createGrid(slider.value);
 })
 
-randomButton.addEventListener("click", () => {
-    let r = Math.floor(Math.random() * 256);
-    let g = Math.floor(Math.random() * 256);
-    let b = Math.floor(Math.random() * 256);
-    hoverColor = `rgb(${r}, ${g}, ${b})`;
-    
+randomButton.addEventListener("click", pickRandomColor);
 
+rainbowButton.addEventListener("click", () => {
+    rainbowMode = true;
+})
+
+regularModeButton.addEventListener("click", () => {
+    rainbowMode = false;
+    hoverColor = "black";
 })
 
 container.addEventListener("mouseover", (event) => {
     let itemHovered = event.target;
-    itemHovered.style.backgroundColor = hoverColor;
+    if (rainbowMode == true) {
+        if (itemHovered.style.backgroundColor == "white") {
+            pickRandomColor()
+            itemHovered.style.backgroundColor = hoverColor;
+        }
+        else {
+            let opca = itemHovered.style.opacity;
+            itemHovered.style.opacity = Number(opca) + 0.1;
+        }
+    }
+    else if (rainbowMode == false) {
+        if (itemHovered.style.backgroundColor == "white") {
+            itemHovered.style.backgroundColor = hoverColor;
+        }
+        else { 
+            let opca = itemHovered.style.opacity;
+            itemHovered.style.opacity = Number(opca) + 0.1;
+        }
+    }
 })
 
 resetButton.addEventListener("click", () => {
